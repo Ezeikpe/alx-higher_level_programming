e04b5fa575e66f03f2c66bdd201ec0d4c5172c80
Ezeikpe <juscraft@yahoo.com>
2023-04-26 22:21:56 -0700

    Exec files

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-//a script that reads and prints the content of a file.
-const fs = require('fs');
 
 const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
+const fs = require('fs');
+fs.readFile(filePath, 'utf8', function (err, data) {
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-// a script that display the status code of a GET request.
-const request = require('request');
 
+const request = require('request');
 const url = process.argv[2];
 
-request.get(url, (error, response) => {
-  if (error) {
-    console.error(error);
-    return;
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
   }
-  console.log(`code: ${response.statusCode}`);
 });
